d5d62fdb08f36ba3df1c50ea90135de01f98565b
softmakers-dev <softmakers.dev@gmail.com>
2022-06-29 22:42:34 +0900

    Q.10950 added

--- src/main/java/com/softmakers/level03/Question10950.java
+++ src/main/java/com/softmakers/level03/Question10950.java
@@ -1,24 +1,32 @@
 package com.softmakers.level03;
 
+/**
+ * Special thanks to Polygenelubricants in Stackoverflow
+ * https://stackoverflow.com/questions/1794281/java-infinite-loop-using-scanner-in-hasnextint
+ */
 import java.util.Scanner;
 
 class Question10950 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int numOfCase = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int numOfCase = scanner.nextInt();
         int[] results = new int[numOfCase];
 
-        Scanner sc2 = new Scanner(System.in);
-        for(int i = 0; i < numOfCase; i++) {
+        for(int i=0; i<numOfCase; i++) {
 
-            int firstNum = sc2.nextInt();
-            int secondNum = sc2.nextInt();
-            results[i] = firstNum + secondNum;
+            if(scanner.hasNextInt()) {
+
+                int firstNum = scanner.nextInt();
+                int secondNum = scanner.nextInt();
+                results[i] = firstNum + secondNum;
+            } else {
+                scanner.next();
+            }
         }
 
-        for(int j = 0; j < numOfCase; j++) {
+        for(int j=0; j<numOfCase; j++) {
 
             System.out.println(results[j]);
         }
