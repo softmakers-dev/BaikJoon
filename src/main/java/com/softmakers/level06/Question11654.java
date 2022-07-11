d437085a285b07b6c46945d5aedd7f396d603eeb
softmakers-dev <softmakers.dev@gmail.com>
2022-07-11 20:54:19 +0900

    level06 started

---
+++ src/main/java/com/softmakers/level06/Question11654.java
@@ -0,0 +1,15 @@
+package com.softmakers.level06;
+
+import java.util.Scanner;
+
+public class Question11654 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String sInput = scanner.next();
+        int asc = sInput.charAt(0);
+        System.out.print(asc);
+    }
+}
